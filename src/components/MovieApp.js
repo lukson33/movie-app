292d138b7c2bd32907fc27a6395fc62c26b12e20
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-08-09 00:12:52 +0200

    Update MovieApp.js

--- src/components/MovieApp.js
+++ src/components/MovieApp.js
@@ -2,16 +2,18 @@ import React, { Component } from "react";
 import Movie from "./Movie";
 import axios from "axios";
 import "../MovieApp.css";
+import { throwStatement } from "@babel/types";
 
 export class MovieApp extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { movieInput: "", movies: [], pages: 1 };
+    this.state = { movieInput: "", movies: [], pages: 1, isShown: true };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleMore = this.handleMore.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleChange = e => {
@@ -19,51 +21,66 @@ export class MovieApp extends Component {
   };
 
   async handleSubmit(e) {
-    e.preventDefault();
-    this.setState({ movieInput: "", movies: [] });
-    const value = this.state.movieInput;
-    const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${value}&page=${
-      this.state.pages
-    }&include_adult=false`;
-    try {
-      const response = await axios.get(url);
-      this.setState({ pages: 2 });
-      console.log(response.data.results);
-      const newMovies = response.data.results;
-      this.setState(prevState => ({
-        movies: [...prevState.movies, newMovies]
-      }));
-      console.log(this.state.movies);
-    } catch (err) {
-      console.log(err);
+    if (this.state.movieInput === "") {
+      e.preventDefault();
+      this.setState({ isShown: true });
+    } else {
+      this.setState({ isShown: false });
+      e.preventDefault();
+      const value = this.state.movieInput;
+      const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${value}&page=1&include_adult=false`;
+      this.setState({
+        movies: []
+      });
+      try {
+        const response = await axios.get(url);
+        this.setState({ pages: 2 });
+        console.log(response.data.results);
+        const newMovies = response.data.results;
+        this.setState(prevState => ({
+          movies: [...prevState.movies, newMovies]
+        }));
+        console.log(this.state.movies);
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
 
   async handleMore(e) {
-    e.preventDefault();
-    const value = this.state.movieInput;
-    const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${value}&page=${
-      this.state.pages
-    }&include_adult=false`;
-    try {
-      const response = await axios.get(url);
-      if (this.state.pages === response.data.total_pages + 1) {
-        console.log("No more results to be shown");
-      } else {
-        const newMovies = response.data.results;
-        this.setState(prevState => ({
-          movies: [...prevState.movies, newMovies],
-          pages: this.state.pages + 1
-        }));
-        console.log(newMovies);
+    if (this.state.movieInput === "") {
+      e.preventDefault();
+    } else {
+      e.preventDefault();
+      const value = this.state.movieInput;
+      const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${value}&page=${
+        this.state.pages
+      }&include_adult=false`;
+      try {
+        const response = await axios.get(url);
+        console.log(response);
+        if (this.state.pages === response.data.total_pages + 1) {
+          console.log("No more results to be shown");
+        } else {
+          const newMovies = response.data.results;
+          this.setState(prevState => ({
+            movies: [...prevState.movies, newMovies],
+            pages: this.state.pages + 1
+          }));
+          console.log(newMovies);
+        }
+        console.log(this.state.movies);
+      } catch (err) {
+        console.log(err);
       }
-      console.log(this.state.movies);
-    } catch (err) {
-      console.log(err);
     }
   }
+
+  handleDelete = e => {
+    this.setState({ movieInput: "" });
+  };
 
   render() {
     return (
@@ -75,10 +92,12 @@ export class MovieApp extends Component {
             type="text"
             value={this.state.movieInput}
             onChange={this.handleChange}
+            onClick={this.handleDelete}
           />
           <button type="submit">Submit</button>
           <button onClick={this.handleMore}>Load More</button>
         </form>
+        {this.state.isShown && <p>Please enter a movie</p>}
         <div className="Movie-container">
           {this.state.movies.map(arr =>
             arr.map(m => <Movie movie={m} key={m.id} />)
