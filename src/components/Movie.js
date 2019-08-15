2e24c5332e6b059e5c42410e7401d3badbd9daa5
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-08-15 16:23:50 +0200

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "../Movie.css";
 
 export class Movie extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="Movie">
         <h2>{this.props.movie.title}</h2>
+        {/* to={{
+              pathname: `/movie/${this.state.movieInput}`,
+              state: { name: this.state.movieInput }
+            }} */}
+        {/* <Link to={`/movies/${this.props.movie.title}/${this.props.movie.id}`}> */}
+        <Link
+          to={{
+            pathname: `/movies/${this.props.movie.title}/${
+              this.props.movie.id
+            }`,
+            state: { movieInfo: this.props }
+          }}
+        >
+          <button>More details</button>
+        </Link>
+
+        {/* </Link> */}
 
         {this.props.movie.poster_path ? (
           <img
