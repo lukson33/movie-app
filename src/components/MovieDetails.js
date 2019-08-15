350cf8c8a6845de90729c97f994b35f20d1d016c
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-08-15 16:23:48 +0200

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,35 +1,42 @@
 import React, { useState, useEffect } from "react";
 
-export default function MovieDetails({ match }) {
-  useEffect(() => {
-    fetchMovie();
-  }, []);
+export default function MovieDetails(props) {
+  // useEffect(() => {
+  //   fetchMovie();
+  // }, []);
 
-  const [movie, setMovie] = useState("");
+  // const [movie, setMovie] = useState("");
 
-  const fetchMovie = async () => {
-    const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
-    const fetchMovie = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${
-        match.params.name
-      }&page=1&include_adult=false`
-    );
-    const movie = await fetchMovie.json();
-    const newMovie = movie.results.find(m => m.id === Number(match.params.id));
-    setMovie(newMovie);
-  };
+  // const fetchMovie = async () => {
+  //   const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
+  //   const fetchMovie = await fetch(
+  //     `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${
+  //       match.params.name
+  //     }&page=1&include_adult=false`
+  //   );
+  //   const movie = await fetchMovie.json();
+  //   const newMovie = movie.results.find(m => m.id === Number(match.params.id));
+  //   setMovie(newMovie);
+  // };
 
-  console.log(movie);
-
+  // console.log(movie);
+  console.log(props);
+  const {
+    id,
+    original_title,
+    overview,
+    popularity,
+    poster_path,
+    release_date,
+    vote_count,
+    vote_average
+  } = props.location.state.movieInfo.movie;
   return (
     <div>
-      <h3>{movie.original_title}</h3>
-      <p>{movie.overview}</p>
-      <img
-        src={`http://image.tmdb.org/t/p/w185//${movie.poster_path}`}
-        alt=""
-      />
-      <p>{movie.vote_average}</p>
+      <h3>{original_title}</h3>
+      <p>{overview}</p>
+      <img src={`http://image.tmdb.org/t/p/w185//${poster_path}`} alt="" />
+      <p>{vote_average}</p>
     </div>
   );
 }
