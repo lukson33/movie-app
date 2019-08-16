2562e0f0becb5e35774c514cb320bbf028a5c1da
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-08-16 20:03:24 +0200

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,10 +3,24 @@ import Actor from "./Actor";
 import "../Actor.css";
 
 export default class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isTrue: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
   //Fix scroll error
   componentDidMount() {
     window.scrollTo(0, 0);
   }
+
+  toggle = () => {
+    this.setState({ isTrue: !this.state.isTrue });
+    console.log(this.state.isTrue);
+  };
   render() {
     const {
       id,
@@ -19,8 +33,13 @@ export default class MovieDetails extends Component {
       vote_average
     } = this.props.location.state.movieInfo.movie;
 
-    const actors = this.props.location.state.actors[0].cast;
-    const newActors = actors.slice(0, 9);
+    let actors = this.props.location.state.actors[0].cast;
+    let half = Math.floor(actors.length / 2);
+
+    let actorsFirst = actors.slice(0, half);
+    let actorsSecond = actors.slice(half, actors.length);
+    // const actors = this.props.location.state.actors[0].cast;
+    // const newActors = actors.slice(0, 9);
     return (
       <div className="Movie-details">
         <h3>{original_title}</h3>
@@ -30,11 +49,18 @@ export default class MovieDetails extends Component {
         <p>{vote_average}</p>
 
         <div className="actors-container">
-          {newActors
+          {actorsFirst
             .filter(actor => actor.profile_path)
             .map(actor => (
               <Actor actor={actor} id={actor.cast_id} />
             ))}
+
+          <button onClick={this.toggle}>Load more actors</button>
+          {this.state.isTrue
+            ? actorsSecond
+                .filter(actor => actor.profile_path)
+                .map(actor => <Actor actor={actor} id={actor.cast_id} />)
+            : null}
         </div>
       </div>
     );
