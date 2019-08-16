e360b240f34192cb4455ecf501411c7e804bf004
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-08-16 19:43:25 +0200

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,30 +1,54 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../Movie.css";
 
-export class Movie extends Component {
+export default class Movie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      actors: []
+    };
+  }
+
+  async componentDidMount() {
+    const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
+    const MOVIE_ID = this.props.movie.id;
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${MOVIE_ID}/credits?api_key=${API_KEY}`
+    );
+    const actors = await res.json();
+    this.setState(prevState => ({
+      actors: [...prevState.actors, actors]
+    }));
+  }
+
+  // const fetchActors = async () => {
+  //   const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
+
+  //   const fetchActors = await fetch(
+  //     ``
+  //   );
+  //   const newActors = await fetchActors.json();
+  //   setActors([...actors, actors]);
+  // };
+
+  // console.log(this.props);
   render() {
-    console.log(this.props);
     return (
       <div className="Movie">
         <h2>{this.props.movie.title}</h2>
-        {/* to={{
-              pathname: `/movie/${this.state.movieInput}`,
-              state: { name: this.state.movieInput }
-            }} */}
-        {/* <Link to={`/movies/${this.props.movie.title}/${this.props.movie.id}`}> */}
         <Link
+          onUpdate={() => window.scrollTo(0, 0)}
           to={{
             pathname: `/movies/${this.props.movie.title}/${
               this.props.movie.id
             }`,
-            state: { movieInfo: this.props }
+            state: { movieInfo: this.props, actors: this.state.actors }
           }}
         >
           <button>More details</button>
         </Link>
-
-        {/* </Link> */}
 
         {this.props.movie.poster_path ? (
           <img
@@ -40,5 +64,3 @@ export class Movie extends Component {
     );
   }
 }
-
-export default Movie;
