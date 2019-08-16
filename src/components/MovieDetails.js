ffe40cfe027d2f90a89eb3d8255c7f0644e4ad95
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-08-16 19:43:22 +0200

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState, useEffect } from "react";
+import Actor from "./Actor";
+import "../Actor.css";
 
-export default function MovieDetails(props) {
-  // useEffect(() => {
-  //   fetchMovie();
-  // }, []);
+export default class MovieDetails extends Component {
+  //Fix scroll error
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+  render() {
+    const {
+      id,
+      original_title,
+      overview,
+      popularity,
+      poster_path,
+      release_date,
+      vote_count,
+      vote_average
+    } = this.props.location.state.movieInfo.movie;
 
-  // const [movie, setMovie] = useState("");
+    const actors = this.props.location.state.actors[0].cast;
+    const newActors = actors.slice(0, 9);
+    return (
+      <div className="Movie-details">
+        <h3>{original_title}</h3>
+        <p>{overview}</p>
+        <p>{release_date}</p>
+        <img src={`http://image.tmdb.org/t/p/w185//${poster_path}`} alt="" />
+        <p>{vote_average}</p>
 
-  // const fetchMovie = async () => {
-  //   const API_KEY = "aa7add1a816db1a576175e4abfc544cf";
-  //   const fetchMovie = await fetch(
-  //     `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${
-  //       match.params.name
-  //     }&page=1&include_adult=false`
-  //   );
-  //   const movie = await fetchMovie.json();
-  //   const newMovie = movie.results.find(m => m.id === Number(match.params.id));
-  //   setMovie(newMovie);
-  // };
-
-  // console.log(movie);
-  console.log(props);
-  const {
-    id,
-    original_title,
-    overview,
-    popularity,
-    poster_path,
-    release_date,
-    vote_count,
-    vote_average
-  } = props.location.state.movieInfo.movie;
-  return (
-    <div>
-      <h3>{original_title}</h3>
-      <p>{overview}</p>
-      <img src={`http://image.tmdb.org/t/p/w185//${poster_path}`} alt="" />
-      <p>{vote_average}</p>
-    </div>
-  );
+        <div className="actors-container">
+          {newActors
+            .filter(actor => actor.profile_path)
+            .map(actor => (
+              <Actor actor={actor} id={actor.cast_id} />
+            ))}
+        </div>
+      </div>
+    );
+  }
 }
